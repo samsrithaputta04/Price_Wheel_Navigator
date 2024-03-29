1d21a9ac2bed9a1012af5e5c8a359ec94555df02
samsrithaputta04 <163509713+samsrithaputta04@users.noreply.github.com>
2024-03-29 19:41:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle as pk
 import streamlit as st
+import sklearn
 model=pk.load(open('model.pkl','rb'))
 
 st.header('Price Wheel Navigator - myMLmodel')
